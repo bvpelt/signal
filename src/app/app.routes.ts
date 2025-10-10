b1aee21b64e556a9a34c743cebd715038b8233d4
Bart van Pelt <bart.vanpelt@gmail.com>
2025-10-10 19:45:23 +0200

    WIP

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { ShomeComponent } from './shome/shome.component';
 import { ShopComponent } from './shop/shop.component';
 import { LoggerComponent } from './logger/logger.component';
+import { OrdercardComponent } from './ordercard/ordercard.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'signal', component: ShomeComponent },
   { path: 'shop', component: ShopComponent },
+  { path: 'orders', component: OrdercardComponent },
   { path: 'log', component: LoggerComponent },
 ];
